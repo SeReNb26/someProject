3b74383cb19760ee8289a6bf324a39d7855784bb
Benito <zara.benito17@gmail.com>
2021-05-13 16:28:55 +0300

    added class AppBenito with method say()

---
+++ src/main/java/AppBenito.java
@@ -0,0 +1,11 @@
+public class AppBenito {
+    public static void main(String[] args) {
+        int a = 15;
+        String name = "something";
+        System.out.println(say());
+    }
+
+    private static String say(){
+        return "Hello!";
+    }
+}
